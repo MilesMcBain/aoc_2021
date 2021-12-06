ee66f9db7cd96299e677760b7229324b49047d2a
Miles McBain <miles.mcbain@gmail.com>
2021-12-06 20:24:24 +1000

    ouch

--- 4/4_1.R
+++ 4/4_1.R
@@ -1,6 +1,6 @@
-input_file <-"./input4_1.txt" 
+BOARD_SIZE <- 5
 
-number_draws <- 
+number_draws <-
   readLines(input_file, n = 1) |>
   strsplit(",") |>
   unlist() |>
@@ -12,34 +12,44 @@ boards <-
     sep = "",
     skip = 1,
     blank.lines.skip = TRUE
-  ) |> 
+  ) |>
   as.matrix() |>
   unname()
 
-dim(boards) <-
-  c(5, 5, nrow(board_data) / 5)
-# it's a 3d array now
 
 boards_results <-
   array(rep(FALSE, length(boards)), dim = dim(boards))
 # Another 3d indicator array
 
-pop_draw <- function(number_draws) {
-        value <- number_draws[[1]]
-        number_draws <<- number_draws[-1]
-        value
+board_num_to_indexes <- function(board_num, .dim = dim(boards)) {
+  row <- rep(seq(BOARD_SIZE), times = BOARD_SIZE) +
+    (BOARD_SIZE * (board_num - 1))
+  col <- rep(seq(BOARD_SIZE), each = BOARD_SIZE)
+
+  cbind(row, col)
+}
+
+board_indexes <- lapply(seq(nrow(boards) / BOARD_SIZE), board_num_to_indexes)
+
+pop_draw <- function(number_draws, .dim = dim(boards)) {
+  value <- number_draws[[1]]
+  number_draws <<- number_draws[-1]
+  value
 }
 
 apply_draw <- function(boards_results, draw) {
   boards_results[boards == draw] <- TRUE
   boards_results
-} 
+}
 
 get_winner <- function(boards_results) {
-  winners <- apply(boards_results, 3,
-    \(board_mat) {
-      any(colSums(board_mat) == 5) | any(rowSums(board_mat) == 5)
-    }) |>
+  winners <- lapply(
+    board_indexes,
+    \(board_ind) {
+      board_mat <- matrix(boards_results[board_ind], nrow = BOARD_SIZE)
+      any(colSums(board_mat) == BOARD_SIZE) | any(rowSums(board_mat) == BOARD_SIZE)
+    }
+  ) |>
     unlist()
   head(which(winners), 1)
 }
@@ -52,7 +62,8 @@ while (length(winner) == 0) {
   winner <<- get_winner(boards_results)
 }
 
-winning_board <- boards[ , , winner]
-unmarked_on_winning_board <- winning_board[!boards_results[ , , winner]]
+winning_board <- boards[board_num_to_indexes(winner)]
+unmarked_on_winning_board <-
+  winning_board[!boards_results[board_num_to_indexes(winner)]]
 
 print(latest_draw * sum(unmarked_on_winning_board))
