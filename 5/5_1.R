8d392b06688540a40d0fc4a648989f1744fcb6d3
Miles McBain <miles.mcbain@gmail.com>
2021-12-08 21:49:10 +1000

    burn those cycles

---
+++ 5/5_1.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+library(readr)
+library(stringr)
+
+expand_points <- function(points_vec) {
+  coordinates <- 
+    as.integer(points_vec)  |>
+    setNames(c("x1", "y1", "x2", "y2"))
+
+  if (coordinates["x1"] != coordinates["x2"] & 
+      coordinates["y1"] != coordinates["y2"]) return(NULL)
+
+  expand.grid(x = seq(coordinates["x1"], coordinates["x2"]), 
+              y = seq(coordinates["y1"], coordinates["y2"]))
+
+} 
+
+points_matrix <-
+  read_lines("input5_1.txt") |>
+  str_extract_all("[0-9]+") |>
+  lapply(expand_points) |>
+  do.call(what = rbind) |>
+  group_by(x, y) |>
+  summarise(
+    times_covered = n(),
+    .groups = "drop"
+  ) |> 
+  filter(
+    times_covered >= 2
+  ) |>
+  nrow()
