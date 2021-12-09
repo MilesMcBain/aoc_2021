0c71184c8ff69169b714b2c294dcf9b6c3054c3c
Miles McBain <miles.mcbain@gmail.com>
2021-12-09 20:04:13 +1000

    spooky big numbers

---
+++ 6/6_1.R
@@ -0,0 +1,36 @@
+library(dplyr)
+library(tidyr)
+options(scipen = 999)
+
+fish_ages <-
+    scan(
+    "input6_1.txt",
+    what = integer(),
+    sep = ","
+  ) |> 
+  table() |> 
+  as.list() |>
+  modifyList(
+    x = setNames(as.list(rep(0, length(seq(-1, 8)))), seq(-1, 8))
+  )
+
+shift_list_left <- function(a_list) {
+  list_names <- names(a_list)
+  c(a_list[-1], 0) |>
+    setNames(list_names)
+}
+
+new_population <-
+  reduce(
+    .x = seq(256),
+    .init = fish_ages,
+    .f = function(fish_ages, epoch) {
+        new_ages <- shift_list_left(fish_ages)
+        reproducing <- new_ages[["-1"]] 
+        new_ages["-1"] <- 0
+        new_ages["8"] <- reproducing
+        new_ages["6"] <- new_ages[["6"]] + reproducing
+        new_ages
+      })
+
+do.call(sum, new_population)
